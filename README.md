083d278f1b1600232b3c59c89cdc020897c08f10
nazimfeni <mdnazimuddin77@gmail.com>
2024-03-16 15:04:52 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# vue-crud-app
+Tutorial sit: https://www.youtube.com/watch?v=KtUV3H9wM7A&list=PL8p2I9GklV45e7L_qC_mntM7FhSIV8pqa&index=51
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/SignUp.vue
@@ -0,0 +1,79 @@
+<template>
+  <img class="logo" src="../assets/resto-logo.jpg" /> <br />
+  
+  <div class="register">
+    <h1>Signup</h1>
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signUp">Signup</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      console.warn("signUp", this.name, this.email, this.password);
+      let result = await axios.post("http://localhost:3000/users", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        alert("Signup Done");
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data));
+    },
+  },
+};
+</script>
+
+<style>
+
+.head{
+
+
+}
+.logo {
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+  width: 150px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+.register input {
+  
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 300px;
+  height: 40px;
+  color: white;
+  background-color: skyblue;
+  border: 1px solid skyblue;
+  cursor: pointer;
+}
+</style>
